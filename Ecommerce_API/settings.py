84c4f902b1599e799bbc9efd5bd5e9dd5ae15469
Yax26 <yaxbarot2605@gmail.com>
2025-04-14 00:40:47 -0400

    Update settings.py

--- Ecommerce_API/settings.py
+++ Ecommerce_API/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'dxisifjGEUckfvFNuqiGRhHsSLpPLiaB',
-        'HOST': 'mysql.railway.internal',
+        'PASSWORD': 'sXCUdAjlfxhEKFDKKTdtjBWJBVaMKnTf',
+        'HOST': 'mysql-clsm.railway.internal',
         'PORT': '3306',
         'OPTION': {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
