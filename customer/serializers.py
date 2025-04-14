1ffd87602ab0c8c7daaf72edacac5cbf49123255
Yax26 <yaxbarot2605@gmail.com>
2025-04-13 22:23:50 -0400

    Empty main branch

--- customer/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from customer.models import Customer
-
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = "__all__"
--- security/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from security.models import CustomerAuthTokens
-
-
-class CustomerAuthTokenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomerAuthTokens
-        fields = "__all__"
