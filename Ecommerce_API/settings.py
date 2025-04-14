9a54b7a74df159378e9dc614f12ac6fc6e663571
Yax26 <yaxbarot2605@gmail.com>
2025-04-13 22:33:52 -0400

    Update settings.py

--- Ecommerce_API/settings.py
+++ Ecommerce_API/settings.py
@@ -14,6 +14,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
